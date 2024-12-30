4b6ad56bcce5186639905999ca07d2dc1442b9f4
SoumyadeepOSD <opencontainer255@gmail.com>
2024-12-30 14:51:27 +0530

    fixed protected route related issue

--- backend/src/middleware/auth-middleware.ts
+++ backend/src/middleware/auth-middleware.ts
@@ -3,10 +3,9 @@ import { ResponseToolkit, Lifecycle } from "@hapi/hapi";
 
 const JWT_SECRET = process.env.JWT_SECRET!;
 
-const authMiddleware = async (req: any, h: ResponseToolkit): Promise<Lifecycle.ReturnValue> => {
+const authMiddleware = async (artifacts:any ,req: any, h: ResponseToolkit): Promise<Lifecycle.ReturnValue> => {
     try {
         const authHeader = req.headers.authorization;
-
         if (!authHeader || !authHeader.startsWith("Bearer ")) {
             return h
                 .response({
@@ -18,19 +17,20 @@ const authMiddleware = async (req: any, h: ResponseToolkit): Promise<Lifecycle.R
 
         const token = authHeader.split(" ")[1];
         const decoded = jwt.verify(token, JWT_SECRET) as any;
-
-        // Attach decoded user info to the request object for further use
-        req.auth = { userId: decoded.id };
-
-        return h.continue;
+        req.auth = { userId: decoded.id }
+        return {
+            isValid: true,
+            credentials: { user: artifacts.decoded.payload.user }
+        };
     } catch (error: any) {
+        // console.log(error);
         return h
             .response({
                 message: "Unauthorized access",
                 error: error.message,
             })
-            .code(401)
-            .takeover();
+            .code(401);
+            // .takeover();
     }
 };
 
